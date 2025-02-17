45c38a4c1dfd1a5f49ee3ca06c93f019a1b7007a
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-17 11:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+let btn = document.querySelector("#btn");
+let form = document.querySelector("#myForm");
+
+btn.addEventListener("click" , () =>{
+	if(age.value==="" || name.value===""){
+   alert("Please enter valid details");
+}
+
+});
+
+
+form.addEventListener("submit" , (event) =>{
+	event.preventDefault(); 
+	let myPromise = new Promise((resolve,reject) =>{;
+	if(age.value>18){
+	  setTimeout(()=>{
+		  resolve("Welcome, . You can vote.");
+},4000);
+	}else {
+			reject("Oh sorry . You aren't old enough.");
+		}
+
+});
+
+	myPromise.then((result)=>{
+		alert(result);
+	}).catch((error) => {
+		alert(error);
+	});
+	
+});
+
